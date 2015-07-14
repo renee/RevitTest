e6641143e4ca53d43fb1950c3423e7ccd0ea601b
renee <renee.puusepp@sliderstudio.co.uk>
2015-07-14 14:32:39 +0300

    Added RVt files and VB macro

---
+++ AppLevelMacro.vb
@@ -0,0 +1,29 @@
+﻿'
+' Created by SharpDevelop.
+' User: ReneeMac
+' Date: 14.07.2015
+' Time: 14:28
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Imports System
+Imports Autodesk.Revit.UI
+Imports Autodesk.Revit.DB
+Imports Autodesk.Revit.UI.Selection
+Imports System.Collections.Generic
+Imports System.Linq
+
+
+<Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)> _
+<Autodesk.Revit.DB.Macros.AddInId("EA80DA8E-DC52-4137-BED1-1B1E9EE378E4")> _
+Partial Public Class ThisApplication
+
+    Private Sub Module_Startup(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Startup
+	
+    End Sub
+	
+    Private Sub Module_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shutdown
+	
+    End Sub
+	
+End Class
